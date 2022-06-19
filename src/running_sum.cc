17bdc271f7a4d450539d32ce304584bf8eb3ee5a
kvzhao <kelispinor@gmail.com>
2022-06-19 22:54:21 +0800

    update: add a stl sol

--- src/running_sum.cc
+++ src/running_sum.cc
@@ -9,7 +9,10 @@ public:
     for (size_t i = 1; i < nums.size(); ++i) {
       nums[i] += nums[i - 1];
     }
-
+    return nums;
+  }
+  vector<int> runningSumSTL(vector<int> &nums) {
+    partial_sum(nums.begin(), nums.end(), nums.begin());
     return nums;
   }
 };
@@ -21,7 +24,7 @@ int main(int argc, char *argv[]) {
 
   while (getline(cin, line)) {
     auto nums = stringToIntegerVector(line);
-    vector<int> ret = sol.runningSum(nums);
+    vector<int> ret = sol.runningSumSTL(nums);
     auto retStr = integerVectorToString(ret);
     cout << retStr << endl;
   }
