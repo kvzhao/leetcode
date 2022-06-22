1eb50b48bcade3155b61d62fd06b85c70c2dc92f
kvzhao <kelispinor@gmail.com>
2022-06-22 23:35:12 +0800

    update: check existance before run test case

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -5,4 +5,9 @@ g++ -std=c++17 -Wall -O2 src/$1.cc
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
-./a.out < tests/$1.txt
+
+if test -f tests/$1.txt; then
+    ./a.out < tests/$1.txt
+else
+    ./a.out
+fi
