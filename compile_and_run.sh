232cbbd2de4b83978a1a4bc95aa6b2efc6f1dd26
kvzhao <kelispinor@gmail.com>
2022-06-26 18:02:23 +0800

    update: verbose compile and run

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "Run task: $1" 
+echo "Compile task: $1" 
 g++ -std=c++17 -Wall -O2 src/$1.cc
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
 if test -f tests/$1.txt; then
+    echo "Run task: $1"
     ./a.out < tests/$1.txt
 else
     ./a.out
