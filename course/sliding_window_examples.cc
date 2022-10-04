9e6e150a36e289a8eb9e8e5db5a40ea2fe031dae
kvzhao <kelispinor@gmail.com>
2022-10-04 23:59:49 +0800

    update example

--- course/sliding_window_examples.cc
+++ course/sliding_window_examples.cc
@@ -40,3 +40,17 @@ int findLengthBitString(string s) {
 
   return ans;
 }
+
+int findBestSubarray(vector<int> &nums, int k) {
+  int sum = 0;
+  for (int i = 0; i < k; ++i)
+    sum += nums[i];
+
+  int ans = sum;
+  for (int i = k; i < nums.size(); ++i) {
+    sum += nums[i] - nums[i - k];
+    ans = max(sum, ans);
+  }
+
+  return ans;
+}
